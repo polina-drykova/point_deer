0e30fd06e1c9d74193cfc9f9605c9ae022fdeb25
Polina Drykova <polina.drykova1988@gmail.com>
2019-12-08 11:05:25 +0700

    styled and layed out indext page

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -7,7 +7,7 @@ Ckeditor.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'ckeditor/orm/active_record'
-    config.cdn_url = "//cdn.ckeditor.com/4.13.0/full/ckeditor.js"
+    config.cdn_url = "//cdn.ckeditor.com/4.13.0/basic/ckeditor.js"
 
 
   # Allowed image file types for upload.
