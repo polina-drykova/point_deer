b5a15ee481586d4219db2584cc94d343928fdf54
Polina Drykova <polina.drykova1988@gmail.com>
2019-12-08 13:09:54 +0700

    reconfig CkeditorPictureUploader

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -2,10 +2,32 @@
 
 class CkeditorPictureUploader < CarrierWave::Uploader::Base
   include Ckeditor::Backend::CarrierWave
+  include CarrierWave::MiniMagick
+  include Cloudinary::CarrierWave
 
+  process :extract_dimensions
+
+  [:extract_content_type, :extract_size, :extract_dimensions].each do |method|
+    define_method :"#{method}_with_cloudinary" do
+      send :"#{method}_without_cloudinary" if self.file.is_a? CarrierWave::SanitizedFile
+      {}
+    end
+    alias_method_chain method, :cloudinary
+  end
+
+  version :thumb do
+    process :resize_to_fill => [118, 100]
+  end
+
+  version :content do
+    process :resize_to_limit => [800, 800]
+  end
+
+  def extension_white_list
+    Ckeditor.image_file_types
+  end
   # Include RMagick or ImageScience support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
@@ -30,17 +52,8 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_fill: [118, 100]
-  end
 
-  version :content do
-    process resize_to_limit: [800, 800]
-  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_white_list
-    Ckeditor.image_file_types
-  end
 end
