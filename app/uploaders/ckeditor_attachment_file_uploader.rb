50bc3a4f25afffbfb6f928b0ebce38f11815da84
Polina Drykova <polina.drykova1988@gmail.com>
2019-12-08 13:42:22 +0700

    changed CkeditorAttachmentFileUploader cofigurations back

--- app/uploaders/ckeditor_attachment_file_uploader.rb
+++ app/uploaders/ckeditor_attachment_file_uploader.rb
@@ -6,14 +6,14 @@ class CkeditorAttachmentFileUploader < CarrierWave::Uploader::Base
   include Ckeditor::Backend::CarrierWave
   include Cloudinary::CarrierWave
 
-  [:extract_content_type, :extract_size, :extract_dimensions].each do |method|
-    define_method :"#{method}_with_cloudinary" do
-      send(:"#{method}_without_cloudinary") if self.file.is_a?(CarrierWave::SanitizedFile)
-      {}
-    end
-    alias_method :"#{method}_without_cloudinary", method
-    alias_method method, :"#{method}_with_cloudinary"
-  end
+  # [:extract_content_type, :extract_size, :extract_dimensions].each do |method|
+  #   define_method :"#{method}_with_cloudinary" do
+  #     send(:"#{method}_without_cloudinary") if self.file.is_a?(CarrierWave::SanitizedFile)
+  #     {}
+  #   end
+  #   alias_method :"#{method}_without_cloudinary", method
+  #   alias_method method, :"#{method}_with_cloudinary"
+  # end
 
   # Include RMagick or ImageScience support:
   # include CarrierWave::RMagick
@@ -21,13 +21,13 @@ class CkeditorAttachmentFileUploader < CarrierWave::Uploader::Base
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  # def store_dir
-  #   "uploads/ckeditor/attachments/#{model.id}"
-  # end
+  def store_dir
+    "uploads/ckeditor/attachments/#{model.id}"
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -35,7 +35,7 @@ class CkeditorAttachmentFileUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :scale => [200, 300]
   #
   # def scale(width, height)
   #   # do something
